8b75b822d6c3262b8500b71194d380b72a02959b
Mike Hogarth <mikehogarth20@hotmail.com>
2022-05-10 21:49:46 +0100

    Add PWA

--- next.config.js
+++ next.config.js
@@ -1,6 +1,12 @@
-module.exports = {
+const withPWA = require("next-pwa");
+
+module.exports = withPWA({
   reactStrictMode: true,
   images: {
     domains: ["cdn.sanity.io"],
   },
-};
+  pwa: {
+    disable: process.env.NODE_ENV === "development",
+    dest: "public",
+  },
+});

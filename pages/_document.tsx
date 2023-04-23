0d108ed64d29a9bfd8320f72e84066b6183a5ccb
Mikey Hogarth <mikehogarth20@hotmail.com>
2023-04-23 10:41:14 +0100

    Update

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -32,7 +32,7 @@ class MyDocument extends Document {
           <link
             rel="preconnect"
             href="https://fonts.gstatic.com"
-            crossOrigin="crossorigin"
+            crossOrigin=""
           />
           <link
             href="https://fonts.googleapis.com/css2?family=Leckerli+One&display=swap"
